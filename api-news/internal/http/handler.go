7532bf1517305618a6379589c20f6f9d721f3c87
Sebas3004tian <sebas3004tian@gmail.com>
2025-11-29 11:44:19 -0500

    feat: can recibe multiple news in /index

--- api-news/internal/http/handler.go
+++ api-news/internal/http/handler.go
@@ -15,28 +15,41 @@ type ArticleHandler struct {
 func NewArticleHandler(e *services.EmbedService) *ArticleHandler {
 	return &ArticleHandler{Embeds: e}
 }
-
 func (h *ArticleHandler) Index(c *fiber.Ctx) error {
-	var article models.Article
+	var articles []models.Article
 
-	if err := c.BodyParser(&article); err != nil {
-		return fiber.ErrBadRequest
+	if err := c.BodyParser(&articles); err != nil {
+		var single models.Article
+		if err2 := c.BodyParser(&single); err2 != nil {
+			return fiber.ErrBadRequest
+		}
+		articles = append(articles, single)
 	}
 
-	text := article.Title + " " + article.Description + " " + article.Content
+	results := make([]fiber.Map, 0)
 
-	vector, err := h.Embeds.EmbedText(text)
-	if err != nil {
-		return err
+	for _, article := range articles {
+		text := article.Title + " " + article.Description + " " + article.Content
+
+		vector, err := h.Embeds.EmbedText(text)
+		if err != nil {
+			return err
+		}
+
+		log.Println("Artículo recibido:")
+		log.Println("ID:", article.ID)
+		log.Println("Title:", article.Title)
+		log.Println("Vector:", vector)
+
+		results = append(results, fiber.Map{
+			"id":     article.ID,
+			"status": "indexed (simulado)",
+			"vector": vector,
+		})
 	}
-
-	log.Println("Artículo recibido:")
-	log.Println("ID: ", article.ID)
-	log.Println("Title:", article.Title)
-	log.Println("Vector:", vector)
 
 	return c.JSON(fiber.Map{
-		"status": "indexed (simulado)",
-		"vector": vector,
+		"count":   len(results),
+		"results": results,
 	})
 }
