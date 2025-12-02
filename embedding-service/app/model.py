162578ffaeb4d0a07ec5c698c0db1a6289a34999
Sebas3004tian <sebas3004tian@gmail.com>
2025-12-02 14:22:00 -0500

    feat: docker compose running but with little problems

--- embedding-service/app/model.py
+++ embedding-service/app/model.py
@@ -1,7 +1,10 @@
 from sentence_transformers import SentenceTransformer
 import os
+from sentence_transformers.util import cos_sim
+
 
 MODEL_NAME = os.getenv("SENTENCE_TRANSFORMER_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
 
 def get_model():
-    return SentenceTransformer(MODEL_NAME)
+    return SentenceTransformer(MODEL_NAME, device="cpu", trust_remote_code=True)
+
