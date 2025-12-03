306207a3259d239285ad282f8b9cbec7dfd979fc
Sebas3004tian <sebas3004tian@gmail.com>
2025-12-03 07:47:42 -0500

    feat: tfstate backend remoto

--- infra/main.tf
+++ infra/main.tf
@@ -8,13 +8,12 @@ terraform {
     }
   }
 
-  # Opcional:
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-tfstate"
-  #   storage_account_name = "tfstateaccount123"
-  #   container_name       = "tfstate"
-  #   key                  = "infra.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "newsPlatform-rg"
+    storage_account_name = "newsplatetfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
